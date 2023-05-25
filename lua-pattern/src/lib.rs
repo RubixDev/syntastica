770c5b96b9116309cc6e1bb52aee9bfe3f8dd881
RubixDev <silas.groh@t-online.de>
2023-05-25 15:51:38 +0200

    docs: edit styling of features in docs

--- lua-pattern/src/lib.rs
+++ lua-pattern/src/lib.rs
@@ -14,7 +14,7 @@
 //! ```
 #![cfg_attr(
     feature = "docs",
-    cfg_attr(doc, doc = ::document_features::document_features!())
+    cfg_attr(doc, doc = ::document_features::document_features!(feature_label = r#"<span class="stab portability"><code>{feature}</code></span>"#))
 )]
 #![cfg_attr(all(doc, CHANNEL_NIGHTLY), feature(doc_auto_cfg))]
 #![warn(rust_2018_idioms)]
--- rsexpr/src/lib.rs
+++ rsexpr/src/lib.rs
@@ -9,7 +9,7 @@
 //! information.
 #![cfg_attr(
     feature = "docs",
-    cfg_attr(doc, doc = ::document_features::document_features!())
+    cfg_attr(doc, doc = ::document_features::document_features!(feature_label = r#"<span class="stab portability"><code>{feature}</code></span>"#))
 )]
 #![cfg_attr(all(doc, CHANNEL_NIGHTLY), feature(doc_auto_cfg))]
 #![warn(rust_2018_idioms)]
