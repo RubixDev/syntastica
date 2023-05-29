ad4013f3e3319427c4c9a7c2047e455b5b3da9b2
RubixDev <silas.groh@t-online.de>
2023-05-29 11:27:15 +0000

    Deploying to gh-pages from @ RubixDev/syntastica@9d7683289d5e723a82d985db0bc65e6c3ad0fc7f 🚀

--- source-files.js
+++ source-files.js
@@ -10,6 +10,7 @@ var sourcesIndex = JSON.parse('{\
 "syntastica_parsers_gitdep":["",[],["lib.rs"]],\
 "syntastica_queries":["",[],["lib.rs"]],\
 "syntastica_themes":["",[],["gruvbox.rs","lib.rs","one.rs"]],\
+"tree_sitter_wasm_build_tool":["",[],["lib.rs"]],\
 "xtask":["",[["codegen",[],["parser_lists.rs","parsers_dep.rs","parsers_gitdep.rs","queries.rs","theme_gruvbox.rs","theme_one.rs"]]],["codegen.rs","main.rs","set_version.rs"]]\
 }');
 createSourceSidebar();
