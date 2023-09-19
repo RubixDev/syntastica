4bd93fbf03378af50dc057594135fe9848bed3fe
RubixDev <silas.groh@t-online.de>
2023-09-19 10:58:59 +0800

    chore: allow clippy lint `needless_raw_string_hashes`

--- rsexpr/tests/display.rs
+++ rsexpr/tests/display.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::needless_raw_string_hashes)]
+
 #[test]
 #[cfg(feature = "comments")]
 fn comments() {
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::needless_raw_string_hashes)]
+
 use std::{
     env,
     path::{Path, PathBuf},
