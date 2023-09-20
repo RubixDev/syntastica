065f779f8af714d50e836a3a46c153123f9dd77c
RubixDev <silas.groh@t-online.de>
2023-09-20 05:52:19 +0000

    Deploying to gh-pages from @ RubixDev/syntastica@c7a3ad827e39a4597ce2dee68e7c7856b6ce0880 🚀

--- demos/dioxus/assets/dioxus/snippets/dioxus-interpreter-js-603636eeca72cf05/inline0.js
+++ demos/dioxus/assets/dioxus/snippets/dioxus-interpreter-js-603636eeca72cf05/inline0.js
@@ -163,7 +163,19 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
       function truthy(val) {
         return val === "true" || val === true;
       }
-    const ns_cache = [];
+    let s = "";let lsp,sp,sl; let c = new TextDecoder();const attr = [];
+                    let attr_tmp1, attr_tmp2;
+                    function get_attr() {
+                        attr_tmp2 = u8buf[u8bufp++];
+                        if(attr_tmp2 & 128){
+                            attr_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
+                            attr[attr_tmp2&4294967167]=attr_tmp1;
+                            return attr_tmp1;
+                        }
+                        else{
+                            return attr[attr_tmp2&4294967167];
+                        }
+                    }const ns_cache = [];
                     let ns_cache_tmp1, ns_cache_tmp2;
                     function get_ns_cache() {
                         ns_cache_tmp2 = u8buf[u8bufp++];
@@ -187,20 +199,8 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return evt[evt_tmp2&4294967167];
                         }
-                    }let s = "";let lsp,sp,sl; let c = new TextDecoder();let u32buf,u32bufp;const attr = [];
-                    let attr_tmp1, attr_tmp2;
-                    function get_attr() {
-                        attr_tmp2 = u8buf[u8bufp++];
-                        if(attr_tmp2 & 128){
-                            attr_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
-                            attr[attr_tmp2&4294967167]=attr_tmp1;
-                            return attr_tmp1;
-                        }
-                        else{
-                            return attr[attr_tmp2&4294967167];
-                        }
-                    }let u8buf,u8bufp;
-            let field,id,event_name,bubbles,ns,value,ptr,len;
+                    }let u32buf,u32bufp;let u8buf,u8bufp;
+            let event_name,ns,value,len,field,ptr,id,bubbles;
             export function create(r){
                 d=r;
             }
