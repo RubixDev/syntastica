a1b56061cbee17f59aef39e7af90fde276167eea
Silas Groh <silas.groh@adesso.de>
2025-03-07 13:51:47 +0100

    fix(js): export more stdlib symbols

--- syntastica-js/build.rs
+++ syntastica-js/build.rs
@@ -1,7 +1,40 @@
 use std::env;
 
+const EXPORTED_FUNCTIONS: &[&str] = &[
+    // custom functions
+    "_add_language",
+    "_highlight",
+    "_process",
+    "_render",
+    "_get_builtin_theme",
+    // stdlib symbols, see https://github.com/tree-sitter/tree-sitter/blob/v0.25.3/lib/src/wasm/stdlib-symbols.txt
+    "_calloc",
+    "_free",
+    "_iswalnum",
+    "_iswalpha",
+    "_iswblank",
+    "_iswdigit",
+    "_iswlower",
+    "_iswspace",
+    "_iswupper",
+    "_iswxdigit",
+    "_malloc",
+    "_memchr",
+    "_memcmp",
+    "_memcpy",
+    "_memmove",
+    "_memset",
+    "_realloc",
+    "_strcmp",
+    "_strlen",
+    "_strncat",
+    "_strncmp",
+    "_strncpy",
+    "_towlower",
+    "_towupper",
+];
+
 const EMSCRIPTEN_FLAGS: &[&str] = &[
-    "-sEXPORTED_FUNCTIONS=_add_language,_highlight,_process,_render,_get_builtin_theme,_free,_malloc",
     "-sEXPORT_ES6=1",
     "-sMODULARIZE=1",
     "-sEXPORTED_RUNTIME_METHODS=stringToNewUTF8,UTF8ToString,getValue,loadWebAssemblyModule",
@@ -16,6 +49,10 @@ const EMSCRIPTEN_FLAGS: &[&str] = &[
 
 fn main() {
     if env::var("TARGET").is_ok_and(|s| s == "wasm32-unknown-emscripten") {
+        println!(
+            "cargo::rustc-link-arg=-sEXPORTED_FUNCTIONS={}",
+            EXPORTED_FUNCTIONS.join(",")
+        );
         for flag in EMSCRIPTEN_FLAGS {
             println!("cargo::rustc-link-arg={flag}");
         }
